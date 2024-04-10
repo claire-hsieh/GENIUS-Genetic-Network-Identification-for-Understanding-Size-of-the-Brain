321cc0a9c0da444bb51ab758ab9b3796c1e7ca11
Claire Hsieh <chsieh018@gmail.com>
2024-04-09 19:04:38 -0700

    up

---
+++ scripts/README.md
@@ -0,0 +1,31 @@
+## `create_matrices.py`
+
+### Description
+	- Matrix class contains:
+		- adjacency matrix (NxN):   
+		relationships between internal nodes and leaves (species)
+		
+		- species matrix (SxS):  
+		relates ensembl_ids (found in tree) with species name
+		
+		- event matrix (Nx3):  
+		labels internal nodes and leaves with "duplication", "speciation", or "leaf"
+		
+		- internal node matrix (IxN):  
+		labels internal nodes with the nodes and leaves that are under them (ie the root would contain all nodes and leaves)
+		
+### Parameters
+	- `tree_str`: phylogenetic tree in NHX format
+	- `species_id_dict`: dictionary with ensembl_ids and species name (found in `data/` folder)
+	- `print_tree=True`: prints tree
+	- `save_files=True`: save matrices described above in h5 file
+	- `output_dir="./out"`: if `save_files` is True, save files in in this directory
+	- `count=1`: if saving files, count will be appended to end of filename
+	
+## Dependencies
+	- `scipy`, `h5py`, `pandas`
+	
+## `create_trees_for_brain_size_data.py`
+
+	- finds trees with some threshold number of species that are in trees and also in the brain size dataset (`gyz043_suppl_Supplement_Data.csv`)
+	- saves trees in `trees/brain`
---
+++ scripts/create_trees_for_brain_size_data.py
@@ -0,0 +1,64 @@
+import os 
+import pandas as pd
+import sys
+from create_matrices import Matrix
+import gzip as gz
+import regex as re
+from io import StringIO
+from Bio import Phylo
+import pandas as pd
+
+
+brain_size_df = pd.read_csv("../data/gyz043_suppl_Supplement_Data.csv", header=0)
+ensembl_df = pd.read_csv("../data/species_id_gene_id.tsv", header=0, delimiter="\t")
+# ensembl_df.columns = ["Binomial", "ensembl_id"]
+brain_size_df = brain_size_df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+ensembl_df = ensembl_df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+ensembl_df.columns = ["Binomial", "species_id", "gene_id", "gene"]
+merged = pd.merge(brain_size_df, ensembl_df,on="Binomial", how="inner")
+brain_size_species = merged["Binomial"].unique()
+species_id = pd.read_csv("../data/species_id_gene_id.tsv", header=None, index_col=1, delimiter="\t")
+species_id.columns = ["species", "gene_id", "gene"]
+species_id.index.name = "species_id"
+species_id_dict = species_id.to_dict()
+species_id_dict = species_id_dict["species"]
+
+# get trees above threshold=50 for number of species in brain size data and in tree
+count_brain = {}
+trees_w_threshold = {i: [] for i in range(50, 65)}
+with gz.open("../data/trees.txt.gz", "r") as f:
+    for line in f:
+        line = line.decode("utf-8").strip()
+        species_in_tree = []
+        tree_str = re.sub(r"\s+", "", line)
+        tree = Phylo.read(StringIO(tree_str), "newick")
+        for clade in tree.get_terminals():
+            try: species_in_tree.append(species_id_dict[clade.name])
+            except: pass
+        num = set(species_in_tree).intersection(brain_size_species)
+        try: 
+            if len(num) >=50: trees_w_threshold[len(num)].append(line)
+        except: pass
+        count_brain[len(num)] = count_brain.get(len(num), 0) + 1   
+
+if not os.path.exists(f"../trees/brain/"):
+    os.makedirs(f"../trees/brain/")
+for key, value in trees_w_threshold.items():
+    filename = f"../trees/brain/trees_{key}.txt"
+    with open(filename, "w") as w:
+        for tree in value:
+            w.write(f"{tree}\n")
+
+for file in os.listdir("../trees/brain/"):
+	print(file)
+    if file.endswith(".txt"):
+        with open(f"../trees/brain/trees_50.txt", "r") as f:
+            trees = f.readlines()
+            i = 0
+            x = str(file).split("_")[1].split(".")[0]
+            output_dir = f"../trees/brain/{x}/"
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            for tree in trees:
+                matrix = Matrix(tree, species_id_dict, print_tree=False, save_files=True, output_dir=output_dir, count=i)
+                i += 1
