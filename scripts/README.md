934b3b1bab2e24c45c3c79b55c779ba8d32fe961
Claire Hsieh <chsieh018@gmail.com>
2024-05-13 17:22:38 -0700

    pest control

--- scripts/README.md
+++ scripts/README.md
@@ -24,7 +24,7 @@
 - `output_dir="./out"`: if `save_files` is True, save files in in this directory
 - `count=1`: if saving files, count will be appended to end of filename
 	
-## Dependencies
+### Dependencies
 
 - `scipy`, `h5py`, `pandas`
 	
@@ -33,3 +33,28 @@
 
 - finds trees with some threshold number of species that are in trees and also in the brain size dataset (`gyz043_suppl_Supplement_Data.csv`)
 - saves trees in `trees/brain`
+
+## `print_h5py_file.py`
+
+- view h5 files of matrices created using files above
+
+Python: 
+
+```Python
+e_df, e_index, e_indptr, e_rows, e_cols = print_h5py_file("../trees/brain/64/events_3.h5", "events")
+a_df, a_index, a_indptr, a_rows, a_cols  = print_h5py_file("../trees/brain/64/adjacency_3.h5", "adjacency")
+s_df, s_index, s_indptr, s_rows, s_cols = print_h5py_file(f"../trees/brain/64/species_3.h5", "species")
+i_df, i_index, i_indptr, i_rows, i_cols = print_h5py_file(f"../trees/brain/64/species_internal_3.h5", "species_internal", cols=s_cols)
+```
+
+R: 
+
+```R
+source_python("scripts/print_h5py_file.py")
+np <- import("numpy", convert = FALSE)
+
+events = print_h5py_file("../trees/brain/64/events_3.h5", "events")
+species = print_h5py_file(f"../trees/brain/64/species_3.h5", "species")
+adjacency = print_h5py_file("../trees/brain/64/adjacency_3.h5", "adjacency")
+species_internal = print_h5py_file("trees/out/adjacency_3.h5", "species_internal", cols=np$array(species[[5]]))
+```
--- scripts/create_matrices.py
+++ scripts/create_matrices.py
@@ -44,6 +44,92 @@ class Matrix:
 
 
     def create_matrices(self, tree_str, species_id_dict, count=1, print_tree=False, save_files=False, output_dir="."):
+        # tree_str = re.sub(r"\s+", "", tree_str)
+        # tree = Phylo.read(StringIO(tree_str), "newick")
+        # node = 0
+        # num_nodes = 0
+        # internal_nodes = 0
+        # terminal_nodes = 0
+        # event_row = []
+        # species_row = [] # ids 
+        # species_col = [] # names
+        # species_internal_row = []
+        
+        # # initialize events, species, and adjacency matrix
+        # for clade in tree.find_clades():
+        #     num_nodes += 1
+        #     if clade.name == None:
+        #         clade.name = str(internal_nodes)
+        #         internal_nodes += 1
+        #         species_internal_row.append(clade.name)
+        #     else:
+        #         terminal_nodes += 1
+        #         species_row.append(clade.name)
+        #         try:
+        #             species_name = species_id_dict[clade.name]
+        #         except:
+        #             species_name = clade.name
+        #         species_col.append(species_name)
+        #     event_row.append(clade.name)
+            
+        # event_columns = ["leaf", "duplication", "speciation"]
+        # events = np.zeros((num_nodes, 3))
+        # species = np.zeros((terminal_nodes, len(species_row))) 
+        # # adjacency matrix
+        # parent = event_row.copy()
+        # adjacency = np.zeros((len(parent), len(parent)))
+        # # internal - species matrix
+        # species_internal = np.zeros((internal_nodes, len(species_row)))
+
+        # # fill out matrices
+        # for clade in tree.find_clades():
+        #     if ':[' in clade.name:
+        #         name, nhx_string = clade.name.split(':[')
+        #         nhx_tags = nhx_string.rstrip(']').split(':')
+        #         nhx_data = {}
+        #         for tag in nhx_tags:
+        #             key, value = tag.split('=')
+        #             nhx_data[key] = value
+        #         clade.name = name
+        #         clade.comment = str(nhx_data)
+
+        #     # Event matrix
+        #     # row = node, column = event type
+        #     if str(clade.name).isdigit(): # internal node
+        #         if "D=Y" in clade.comment: # duplication
+        #             events[node] = [0, 1, 0]
+        #         elif "D=N" in clade.comment: # speciation
+        #             events[node] = [0, 0, 1]
+        #         else: # idk, throw a warning?
+        #             break
+        #     else: # leaf node
+        #         events[node] = [1, 0, 0]
+
+        #     # Species matrix
+        #     # row = species, column = species name
+        #     if clade.name in species_row:
+        #         try:
+        #             species_name = species_id_dict[clade.name]
+        #             species[species_row.index(clade.name), species_col.index(species_name)] = 1
+        #         except:
+        #             pass # species isn't defined in SEQ, will not have entry in species, [FIX LATER]            
+        #     node += 1
+        # # Adjacency matrix
+        # # row = parent, column = child
+        # for clade in tree.get_nonterminals():
+        #     parent_node = clade.name
+        #     children = [child.name for child in clade.clades]
+        #     for child in children:
+        #         adjacency[parent.index(parent_node), parent.index(child)] = 1
+
+        # # Fill out species-internal node matrix
+        # # row = internal node, column = species_row
+        # adjacency_df = pd.DataFrame(adjacency, index=parent, columns=parent)
+        # for leaf in tree.get_terminals():
+        #     species_name = species_id_dict[leaf.name]
+        #     self.get_ancestors(leaf.name, species_name, species_internal, species_internal_row, species_col, adjacency_df)
+        
+    # remove spaces from tree_str
         tree_str = re.sub(r"\s+", "", tree_str)
         tree = Phylo.read(StringIO(tree_str), "newick")
         node = 0
@@ -61,7 +147,8 @@ class Matrix:
             if clade.name == None:
                 clade.name = str(internal_nodes)
                 internal_nodes += 1
-                species_internal_row.append(clade.name)
+                if clade.name not in species_internal_row:
+                    species_internal_row.append(clade.name)
             else:
                 terminal_nodes += 1
                 species_row.append(clade.name)
@@ -69,17 +156,18 @@ class Matrix:
                     species_name = species_id_dict[clade.name]
                 except:
                     species_name = clade.name
-                species_col.append(species_name)
+                if species_name not in species_col:
+                    species_col.append(species_name)
             event_row.append(clade.name)
             
         event_columns = ["leaf", "duplication", "speciation"]
         events = np.zeros((num_nodes, 3))
-        species = np.zeros((terminal_nodes, len(species_row))) 
+        species = np.zeros((len(species_row), len(species_col))) 
         # adjacency matrix
         parent = event_row.copy()
         adjacency = np.zeros((len(parent), len(parent)))
         # internal - species matrix
-        species_internal = np.zeros((internal_nodes, len(species_row)))
+        species_internal = np.zeros((internal_nodes, len(species_col)))
 
         # fill out matrices
         for clade in tree.find_clades():
@@ -123,12 +211,13 @@ class Matrix:
                 adjacency[parent.index(parent_node), parent.index(child)] = 1
 
         # Fill out species-internal node matrix
-        # row = internal node, column = species_row
+        # row = internal node, column = species
         adjacency_df = pd.DataFrame(adjacency, index=parent, columns=parent)
         for leaf in tree.get_terminals():
             species_name = species_id_dict[leaf.name]
             self.get_ancestors(leaf.name, species_name, species_internal, species_internal_row, species_col, adjacency_df)
         
+
         # Draw tree
         # add speciation / duplication events
         if print_tree:
